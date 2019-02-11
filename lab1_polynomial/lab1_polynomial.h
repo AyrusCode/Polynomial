e38c2a5405725e22224b1934f3445d365aa0b42a
Surya Pandiaraju <surya.pandiaraju@hotmail.com>
2019-02-10 20:57:03 -0500

    Update lab1_polynomial.h

--- lab1_polynomial/lab1_polynomial.h
+++ lab1_polynomial/lab1_polynomial.h
@@ -1,3 +1,10 @@
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
 
 #ifndef lab1_polynomial
 #define lab1_polynomial
@@ -75,16 +82,22 @@ public:
 		}
 
 		try {
-			poly_stream >> value;
-			negative_polynomial.push_back(data[0]);
-			for (int i = 0; i < data.size() - 1; i++) {
-				data[i] = data[i + 1];
+			while (!poly_stream.fail()) {
+				poly_stream >> value;
+				negative_polynomial.push_back(data[0]);
+				for (int i = 0; i < data.size() - 1; i++) {
+					data[i] = data[i + 1];
+				}
+				data[data.size() - 1] = value;
 			}
-			data[data.size() - 1] = value;
+			
 		}
 		catch(...){
+			cout << "Doesnt workskdfkaksd";
 			poly_stream.close();
 		}
+
+		poly_stream.close();
 	
 
 		
